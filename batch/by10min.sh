28d753297766cf57c0feafc6bbb8b99801a53a8c
hidetobara <hidetobara@gmail.com>
2012-11-06 04:36:55 +0900

    change php path

--- batch/by10min.sh
+++ batch/by10min.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/baraoto/VolatileTwit/batch/
 echo "" > min10.log
-/usr/local/bin/php crawl_status.php >> min10.log
-/usr/local/bin/php talk.php >> min10.log
+/usr/bin/php crawl_status.php >> min10.log
+/usr/bin/php talk.php >> min10.log
